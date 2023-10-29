c7bcaa26f0da2d9bb2fe93b44d7ed54403aef438
sachindhamija <sdhamija728@gmail.com>
2023-10-29 16:34:57 +0530

    ReduxToolkit

---
+++ ReduxToolkit/ReducToolkit/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import ToDos from './components/ToDos'
+
+function App() {
+  return (
+    <>
+  <AddTodo />
+  <ToDos />
+    </>
+
+  )
+}
+
+export default App
